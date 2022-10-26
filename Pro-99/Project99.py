cb77a6a13bcf34ee12febc9f89bf976acef52942
Sudhanshu Ganesh A <76681877+Sudhi-A@users.noreply.github.com>
2022-10-26 15:50:53 +0530

    Add files via upload

---
+++ Pro-99/Project99.py
@@ -0,0 +1,13 @@
+name=(input("Enter your name :  "))
+print(name)
+
+iteration = len(name)
+for i in range(0,iteration):
+    for j in range(0,iteration):
+      if(i==j):
+        print(name[j],sep=" ",end=" ")
+      else:
+        print("*",sep=" ",end=" ")
+    print()
+        
+        
